e1f8a77d407e769460ba0aa58b24aa58a461a198
Maxim Elushev <maxim.elushev@yandex.ru>
2022-04-12 23:12:45 +0300

    OP

---
+++ Lab6/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace test_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRE = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Swap = new System.Windows.Forms.Button();
+            this.Creat = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonRE
+            // 
+            this.buttonRE.Location = new System.Drawing.Point(0, 392);
+            this.buttonRE.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonRE.Name = "buttonRE";
+            this.buttonRE.Size = new System.Drawing.Size(292, 31);
+            this.buttonRE.TabIndex = 10;
+            this.buttonRE.Text = "RemoveAll";
+            this.buttonRE.UseVisualStyleBackColor = true;
+            this.buttonRE.Click += new System.EventHandler(this.buttonRE_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(80, 203);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(131, 20);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "Переворот овала";
+            // 
+            // Swap
+            // 
+            this.Swap.Location = new System.Drawing.Point(0, 227);
+            this.Swap.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Swap.Name = "Swap";
+            this.Swap.Size = new System.Drawing.Size(292, 31);
+            this.Swap.TabIndex = 31;
+            this.Swap.Text = "Поворот";
+            this.Swap.UseVisualStyleBackColor = true;
+            this.Swap.Click += new System.EventHandler(this.Swap_Click);
+            // 
+            // Creat
+            // 
+            this.Creat.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Creat.Location = new System.Drawing.Point(0, 0);
+            this.Creat.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Creat.Name = "Creat";
+            this.Creat.Size = new System.Drawing.Size(292, 37);
+            this.Creat.TabIndex = 32;
+            this.Creat.Text = "Start";
+            this.Creat.UseVisualStyleBackColor = true;
+            this.Creat.Click += new System.EventHandler(this.Creat_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 81);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(292, 31);
+            this.button1.TabIndex = 33;
+            this.button1.Text = "Стирание фигур";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Круг",
+            "Квадрат",
+            "Линия",
+            "Ромб",
+            "Прямоугольник",
+            "Овал"});
+            this.comboBox1.Location = new System.Drawing.Point(0, 45);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(292, 28);
+            this.comboBox1.TabIndex = 34;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 120);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(292, 31);
+            this.button2.TabIndex = 35;
+            this.button2.Text = "Показ фигур";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 159);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(292, 31);
+            this.button3.TabIndex = 36;
+            this.button3.Text = "Базовая точка";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 314);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(292, 31);
+            this.button4.TabIndex = 37;
+            this.button4.Text = "Стирание всех фигур";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(0, 353);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(292, 31);
+            this.button5.TabIndex = 38;
+            this.button5.Text = "Показ всех";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(0, 275);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(292, 31);
+            this.button6.TabIndex = 39;
+            this.button6.Text = "Перемещение всех";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.Creat);
+            this.panel1.Controls.Add(this.buttonRE);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.Swap);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(1103, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(292, 892);
+            this.panel1.TabIndex = 43;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1395, 892);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonRE;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button Swap;
+        private System.Windows.Forms.Button Creat;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Lab6/Form1.cs
@@ -0,0 +1,504 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace test_2
+{
+	public partial class Form1 : Form
+	{
+        readonly Figures[] _FiguresAll = new Figures[50]; //объявление массива
+        readonly Random rnd = new Random();
+		//счётчик общий массива----------------------------------------------------------------
+		public int FiguresAllScore = 0;
+		//-------------------------------------------------------------------------------------
+		public Form1()
+		{
+			InitializeComponent();
+			DoubleBuffered = true;
+
+			Paint += MainForm_Paint;
+
+			KeyDown += PressKey;
+		}
+		//-------------------------------------------------------------------------------------
+		void MainForm_Paint(object sender, PaintEventArgs e)              //обновление на форме
+		{
+			for (int i = 0; i < FiguresAllScore; i++)
+			{
+				if (_FiguresAll[i].Visible)
+				{
+					_FiguresAll[i].Fill(e.Graphics, _FiguresAll[i].Brush ?? Brushes.Yellow);
+					_FiguresAll[i].Draw(e.Graphics, _FiguresAll[i].Pen ?? Pens.Black);
+				}
+			}
+		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			this.Refresh();
+		}
+		void RandCord(Graphics g)
+		{
+
+			if (_FiguresAll[FiguresAllScore] is Ellipse || _FiguresAll[FiguresAllScore] is Oval)
+			{
+				_FiguresAll[FiguresAllScore].Move(rnd.Next(0, 900), rnd.Next(0, 500), g, 1);
+			}
+			else
+			{
+				_FiguresAll[FiguresAllScore].Move(rnd.Next(0, 900), rnd.Next(0, 500), g, 2);
+			}
+		}
+		private void Creat_Click(object sender, EventArgs e)
+		{
+			Graphics t = CreateGraphics();
+
+			for (int OGO = 0; OGO < 13; OGO++)
+			{
+				switch (rnd.Next(0, 6))
+				{
+					case 0:
+
+						_FiguresAll[FiguresAllScore] = (new Ellipse());
+						_FiguresAll[FiguresAllScore].Creat();
+						
+
+						for (int i = 0; i <= FiguresAllScore; i++)                      
+						{            
+							_FiguresAll[i].Fill(t, _FiguresAll[i].Brush ?? Brushes.Yellow);
+							_FiguresAll[i].Draw(t, _FiguresAll[i].Pen ?? Pens.Black);
+						}
+						RandCord(t);
+						FiguresAllScore++;
+						this.Refresh();
+						break;
+					case 1:
+
+						_FiguresAll[FiguresAllScore] = (new Polygon());
+						_FiguresAll[FiguresAllScore].Creat();
+						
+
+						for (int i = 0; i <= FiguresAllScore; i++)                            //обновление на форме
+						{
+							_FiguresAll[i].Fill(t, _FiguresAll[i].Brush ?? Brushes.Yellow);
+							_FiguresAll[i].Draw(t, _FiguresAll[i].Pen ?? Pens.Black);
+						}
+
+						RandCord(t);
+						FiguresAllScore++;
+						this.Refresh();
+						break;
+					case 2:
+
+						_FiguresAll[FiguresAllScore] = (new Line());
+						_FiguresAll[FiguresAllScore].Creat();
+						
+
+						for (int i = 0; i <= FiguresAllScore; i++)                          //обновление на форме
+						{
+							_FiguresAll[i].Fill(t, _FiguresAll[i].Brush ?? Brushes.Yellow);
+							_FiguresAll[i].Draw(t, _FiguresAll[i].Pen ?? Pens.Black);
+						}
+
+						RandCord(t);
+						FiguresAllScore++;
+						this.Refresh();
+						break;
+					case 3:
+
+						_FiguresAll[FiguresAllScore] = (new Oval());
+						_FiguresAll[FiguresAllScore].Creat();
+
+
+						for (int i = 0; i <= FiguresAllScore; i++)                       //обновление на форме
+						{            //костыль т.к не смог создать переменную подходящую под PaintEventArgs
+							_FiguresAll[i].Fill(t, _FiguresAll[i].Brush ?? Brushes.Yellow);
+							_FiguresAll[i].Draw(t, _FiguresAll[i].Pen ?? Pens.Black);
+						}
+
+						RandCord(t);
+						FiguresAllScore++;
+						this.Refresh();
+						break;
+					case 4:
+						_FiguresAll[FiguresAllScore] = (new Romb());
+						_FiguresAll[FiguresAllScore].Creat();
+
+						for (int i = 0; i <= FiguresAllScore; i++)                            //обновление на форме
+						{
+							_FiguresAll[i].Fill(t, _FiguresAll[i].Brush ?? Brushes.Yellow);
+							_FiguresAll[i].Draw(t, _FiguresAll[i].Pen ?? Pens.Black);
+						}
+
+						RandCord(t);
+						FiguresAllScore++;
+						this.Refresh();
+						break;
+					case 5:
+						_FiguresAll[FiguresAllScore] = (new Quadrilateral());
+						_FiguresAll[FiguresAllScore].Creat();
+
+
+						for (int i = 0; i <= FiguresAllScore; i++)                            //обновление на форме
+						{
+							_FiguresAll[i].Fill(t, _FiguresAll[i].Brush ?? Brushes.Yellow);
+							_FiguresAll[i].Draw(t, _FiguresAll[i].Pen ?? Pens.Black);
+						}
+
+						RandCord(t);
+						FiguresAllScore++;
+						this.Refresh();
+						break;
+				}	
+			}
+			this.Refresh();
+			MessageBox.Show("Фигура создана", "Создание фигуры", MessageBoxButtons.OK);
+		}
+
+		private void Swap_Click(object sender, EventArgs e)
+		{
+
+			Graphics h = CreateGraphics();
+			int _x, _y, _w, _h;
+
+			for (int i = 0; i < FiguresAllScore; i++)     //проверка на выделенную фигуру
+			{
+				if (_FiguresAll[i] is Oval)
+				{
+					Rectangle _rect = _FiguresAll[i].GetRectangle();
+					_x = _FiguresAll[i].GetX();
+					_y = _FiguresAll[i].GetY();
+					_w = _FiguresAll[i].GetW();
+					_h = _FiguresAll[i].GetH();
+
+					if (_FiguresAll[i] is Oval) 
+					{
+						_FiguresAll[i].GetRectangle();
+						if (_FiguresAll[i].GetRectangle().Width > _FiguresAll[i].GetRectangle().Height)
+						{
+							(_FiguresAll[i] as Oval).Swap(_x, _y, _w, _h, 1);
+
+							_FiguresAll[i].Creat(_FiguresAll[i].GetRectangle());
+							_FiguresAll[i].Move(_rect.X - (_w / 2), _rect.Y - (_w / 2), h, 1);
+						}
+						else if (_FiguresAll[i].GetRectangle().Width < _FiguresAll[i].GetRectangle().Height)
+						{
+							(_FiguresAll[i] as Oval).Swap(_x, _y, _w, _h, 2);
+							_FiguresAll[i].Creat(_FiguresAll[i].GetRectangle());
+							_FiguresAll[i].Move(_rect.X - (_w / 2), _rect.Y - (_w / 2), h, 1);
+						}
+					}
+
+					this.Refresh();                                     //обновление формы
+					MessageBox.Show("Фигура изменена", "Изменение фигуры", MessageBoxButtons.OK);
+				}
+
+			}
+		}
+
+		private void buttonRE_Click(object sender, EventArgs e)//удаление
+		{
+			for (int i = 0; i < FiguresAllScore; i++)
+			{
+				_FiguresAll[i] = null;
+
+			}
+			FiguresAllScore = 0;
+
+			this.Refresh();
+			MessageBox.Show("Фигуры удалены", "удаление фигуры", MessageBoxButtons.OK);
+
+		}
+
+        private void button1_Click(object sender, EventArgs e)//стирание
+        {
+			Graphics g=CreateGraphics();
+			Dele(g);
+			INAN(false);
+
+		}
+		private void Dele(Graphics g)//полное стиранеи
+		{
+			Color color = Color.FromName("White");
+			g.Clear(color);
+		}
+
+        private void button2_Click(object sender, EventArgs e)//показ
+        {
+			INAN(true);
+		}
+		private void INAN(bool CH) //сравнение
+		{
+			if (comboBox1.SelectedItem == null)
+			{
+				MessageBox.Show("Значение не выбрано", "Выбор", MessageBoxButtons.OK);
+				return;
+			}
+			string Indexe = comboBox1.SelectedItem.ToString();
+			if (Indexe == "Круг")
+			{
+				for (int i = 0; i < FiguresAllScore; i++)
+				{
+					if (_FiguresAll[i] is Ellipse && !(_FiguresAll[i] is Oval))
+					{
+						(_FiguresAll[i] as Ellipse).Visible = CH;
+					}
+					this.Refresh();
+				}
+			}
+			else if (Indexe == "Квадрат")
+			{
+				for (int i = 0; i < FiguresAllScore; i++)
+				{
+					if (_FiguresAll[i] is Polygon && !(_FiguresAll[i] is Romb) && !(_FiguresAll[i] is Quadrilateral))
+					{
+						(_FiguresAll[i] as Polygon).Visible = CH;
+					}
+					this.Refresh();
+				}
+			}
+			else if (Indexe == "Линия")
+			{
+				for (int i = 0; i < FiguresAllScore; i++)
+				{
+					if (_FiguresAll[i] is Line)
+					{
+						(_FiguresAll[i] as Line).Visible = CH;
+					}
+					this.Refresh();
+				}
+			}
+			else if (Indexe == "Ромб")
+			{
+				for (int i = 0; i < FiguresAllScore; i++)
+				{
+					if (_FiguresAll[i] is Romb)
+					{
+						(_FiguresAll[i] as Romb).Visible = CH;
+					}
+					this.Refresh();
+				}
+			}
+			else if (Indexe == "Прямоугольник")
+			{
+				for (int i = 0; i < FiguresAllScore; i++)
+				{
+					if (_FiguresAll[i] is Quadrilateral)
+					{
+						(_FiguresAll[i] as Quadrilateral).Visible = CH;
+					}
+					this.Refresh();
+				}
+			}
+			else if (Indexe == "Овал")
+			{
+				for (int i = 0; i < FiguresAllScore; i++)
+				{
+					if (_FiguresAll[i] is Oval)
+					{
+						(_FiguresAll[i] as Oval).Visible = CH;
+					}
+					this.Refresh();
+				}
+			}
+		}
+
+        private void button4_Click(object sender, EventArgs e)//стиранеи всех
+        {
+			Graphics g = CreateGraphics();
+			Dele(g);
+			for (int i = 0; i < FiguresAllScore; i++) 
+			{
+				_FiguresAll[i].Visible = false;
+			}
+			this.Refresh();
+        }
+
+			private void button5_Click(object sender, EventArgs e)//показ всех
+			{
+
+				for (int i = 0; i < FiguresAllScore; i++)
+				{
+					_FiguresAll[i].Visible = true;
+				}
+				this.Refresh();
+			}
+
+			private void button3_Click(object sender, EventArgs e)//базовая точка для класса
+			{
+				Graphics g = CreateGraphics();
+				if (comboBox1.SelectedItem == null) 
+				{
+					MessageBox.Show("Значение не выбрано", "Выбор", MessageBoxButtons.OK);
+					return;
+				}
+				//оператор is – проверяет, совпадает ли тип выражения с заданным типом данных;
+				//оператор as – предназначен для избежания возникновения исключительной ситуации в случае неудачного приведения типов;
+				string Indexe = comboBox1.SelectedItem.ToString();
+				if (Indexe == "Круг")
+				{
+					for (int i = 0; i < FiguresAllScore; i++)
+					{
+						Rectangle _rect=_FiguresAll[i].GetRectangle();
+						if (_FiguresAll[i] is Ellipse && !(_FiguresAll[i] is Oval))
+						{
+							(_FiguresAll[i] as Ellipse).Move(100-_rect.X, 100-_rect.Y, g, 1);
+						}
+						this.Refresh();
+					}
+				}
+			
+				else if (Indexe == "Квадрат")
+				{
+					for (int i = 0; i < FiguresAllScore; i++)
+					{
+						Point[] _points = _FiguresAll[i].GetPoint();
+						if (_FiguresAll[i] is Polygon && !(_FiguresAll[i] is Romb) && !(_FiguresAll[i] is Quadrilateral))
+						{
+							(_FiguresAll[i] as Polygon).Move(200-_points[0].X, 200-_points[0].Y, g, 2);
+						}
+						this.Refresh();
+					}
+				}
+				else if (Indexe == "Линия")
+				{
+					for (int i = 0; i < FiguresAllScore; i++)
+					{
+						Point[] _points = _FiguresAll[i].GetPoint();
+						if (_FiguresAll[i] is Line)
+						{
+							(_FiguresAll[i] as Line).Move(400 - _points[0].X, 400 - _points[0].Y, g, 2);
+						}
+						this.Refresh();
+					}
+				}
+				else if (Indexe == "Ромб")
+				{
+					for (int i = 0; i < FiguresAllScore; i++)
+					{
+						Point[] _points = _FiguresAll[i].GetPoint();
+						if (_FiguresAll[i] is Romb)
+						{
+							(_FiguresAll[i] as Romb).Move(100 - _points[0].X, 400 - _points[0].Y, g, 2);
+						}
+						this.Refresh();
+					}
+				}
+				else if (Indexe == "Прямоугольник")
+				{
+					for (int i = 0; i < FiguresAllScore; i++)
+					{
+						Point[] _points = _FiguresAll[i].GetPoint();
+						if (_FiguresAll[i] is Quadrilateral)
+						{
+							(_FiguresAll[i] as Quadrilateral).Move(400 - _points[0].X, 100 - _points[0].Y, g, 2);
+						}
+						this.Refresh();
+					}
+				}
+				else if (Indexe == "Овал")
+				{
+					for (int i = 0; i < FiguresAllScore; i++)
+					{
+						Rectangle _rect = _FiguresAll[i].GetRectangle();
+						if (_FiguresAll[i] is Oval)
+						{
+							(_FiguresAll[i] as Oval).Move(500-_rect.X, 300-_rect.Y, g, 1);
+						}
+						this.Refresh();
+					}
+				}
+			}
+
+			private void button6_Click(object sender, EventArgs e)//базовая точка для всех
+			{
+				Graphics g = CreateGraphics();
+				for (int i = 0; i < FiguresAllScore; i++)
+				{
+					if (_FiguresAll[i] is Ellipse || _FiguresAll[i] is Oval)
+					{
+						Rectangle _rect = _FiguresAll[i].GetRectangle();
+						_FiguresAll[i].Move(500 - _rect.X, 500 - _rect.Y, g, 1);
+						this.Refresh();
+					}
+					else
+					{
+						Point[] _points = _FiguresAll[i].GetPoint();
+						_FiguresAll[i].Move(500 - _points[0].X, 500 - _points[0].Y, g, 2);
+						this.Refresh();
+					}
+				}
+			}
+
+			private void PressKey(object sender, KeyEventArgs e)
+			{
+
+				Graphics g = CreateGraphics();
+
+				if (e.KeyCode == Keys.Up)
+				{
+					for (int i = 0; i < FiguresAllScore; i++)
+					{
+						if (_FiguresAll[i] is Ellipse || _FiguresAll[i] is Oval)
+						{
+							_FiguresAll[i].Move(0,-24, g, 1);
+							this.Refresh();
+						}
+						else
+						{
+							_FiguresAll[i].Move(0, -24, g, 2);
+							this.Refresh();
+						}
+					}
+				}
+				else if (e.KeyCode == Keys.Down)
+				{
+					for (int i = 0; i < FiguresAllScore; i++)
+					{
+						if (_FiguresAll[i] is Ellipse || _FiguresAll[i] is Oval)
+						{
+							_FiguresAll[i].Move(0, 24, g, 1);
+							this.Refresh();
+						}
+						else
+						{
+							_FiguresAll[i].Move(0, 24, g, 2);
+							this.Refresh();
+						}
+					}
+				}
+				else if (e.KeyCode == Keys.Right)
+				{
+					for (int i = 0; i < FiguresAllScore; i++)
+					{
+						if (_FiguresAll[i] is Ellipse || _FiguresAll[i] is Oval)
+						{
+							_FiguresAll[i].Move(24, 0, g, 1);
+							this.Refresh();
+						}
+						else
+						{
+							_FiguresAll[i].Move(24, 0, g, 2);
+							this.Refresh();
+						}
+					}
+				}
+				else if (e.KeyCode == Keys.Left)
+				{
+					for (int i = 0; i < FiguresAllScore; i++)
+					{
+						if (_FiguresAll[i] is Ellipse || _FiguresAll[i] is Oval)
+						{
+							_FiguresAll[i].Move(-24, 0, g, 1);
+							this.Refresh();
+						}
+						else
+						{
+							_FiguresAll[i].Move(-24, 0, g, 2);
+							this.Refresh();
+						}
+					}
+				}
+			}
+		}
+	}
